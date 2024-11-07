da16269ec1b87ea2385f4ddfb5d8b01f6a0aa63e
nayan <www.xil187@gmail.com>
2024-11-07 16:29:42 +0530

    project created successfully

---
+++ EXperiment/src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import Spline from '@splinetool/react-spline';
+
+export default function Home() {
+    return (
+        <div>
+             <Spline scene="https://prod.spline.design/pno6nsADnLm8lQGM/scene.splinecode" />
+        </div>
+    );
+}
